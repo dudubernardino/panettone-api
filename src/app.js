ed68f9138fda074869d38e79e587931d54b2153c
Eduardo Bernardino <eduardomoura.moura@gmail.com>
2020-10-19 17:00:43 -0300

    Cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 class App {
   constructor() {
@@ -11,6 +12,7 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(cors());
   }
 
   routes() {
