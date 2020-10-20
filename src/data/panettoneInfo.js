e52b426e6b550839a90312f5c4f830667afa30b1
Eduardo Bernardino <eduardomoura.moura@gmail.com>
2020-10-20 14:06:04 -0300

    New infos

--- src/data/panettoneInfo.js
+++ src/data/panettoneInfo.js
@@ -1,13 +1,158 @@
 const panettones = [{
   id: 1,
-  name: 'bauduco'
+  name: 'Panettone bauducco 500g',
+  price: '21,99',
+  brand: 'Bauducco',
+  imgUrl: 'https://a-static.mlcdn.com.br/618x463/panettone-bauducco-500g/rickdoces/9226991172/6e1bb6e9d1af7ddbefb4c8a6ea0afd1e.jpg',
+  nutriInfo: {
+    carb: '39,00g',
+    protein: '5,00g',
+    saturatedFat: '5,10g',
+    totalFat: '10,00g',
+    fiber: '1,80g',
+    sodium: '112,00mg'
+  }
 }, {
   id: 2,
-  name: 'richester'
+  name: 'Panettone Gotas de Chocolate Belga 400g',
+  price: '21,99',
+  brand: 'Santa Edwiges',
+  imgUrl: 'https://a-static.mlcdn.com.br/618x463/panettone-gotas-de-chocolate-belga-400g-santa-edwiges/docemalu/16285/e7f267b7e787230dffb5234213462c83.jpg',
+  nutriInfo: {
+    carb: '42,00g',
+    protein: '7,00g',
+    saturatedFat: '6,90g',
+    totalFat: '15,00g',
+    fiber: '1,80g',
+    sodium: '125,00mg'
+  }
 },
 {
   id: 3,
-  name: 'maizena'
+  name: 'Panettone Gotas de Chocolate Lata 400g',
+  price: '28,29',
+  brand: 'Santa Edwiges',
+  imgUrl: 'https://a-static.mlcdn.com.br/618x463/panettone-gotas-de-chocolate-lata-400g-santa-edwiges/docemalu/16286/dd6d654bef97f77209c601c422368ce9.jpg',
+  nutriInfo: {
+    carb: '40,00g',
+    protein: '5,50g',
+    saturatedFat: '5,20g',
+    totalFat: '11,00g',
+    fiber: '1,90g',
+    sodium: '111,00mg'
+  }
+},{
+  id: 4,
+  name: 'Mini Panettone Gotas de Chocolate Peppa Pig 80g',
+  price: '4,99',
+  brand: 'Santa Edwiges',
+  imgUrl: 'https://a-static.mlcdn.com.br/618x463/mini-panettone-gotas-de-chocolate-peppa-pig-80g-santa-edwiges/docemalu/16316/cf76ad0ac9c0080778fe928c40a29daf.jpg',
+  nutriInfo: {
+    carb: '37,00g',
+    protein: '4,80g',
+    saturatedFat: '5,40g',
+    totalFat: '9,00g',
+    fiber: '1,90g',
+    sodium: '114,00mg'
+  }
+}, {
+  id: 5,
+  name: 'Panettone Nestlé Prestígio 400g',
+  price: '27,74',
+  brand: 'Nestle',
+  imgUrl: 'https://a-static.mlcdn.com.br/618x463/panettone-nestle-prestigio-400g-nestle/lojasemix/1111p/181a25b470424ec379ce23a380baeb4e.jpg',
+  nutriInfo: {
+    carb: '39,00g',
+    protein: '5,00g',
+    saturatedFat: '5,10g',
+    totalFat: '10,00g',
+    fiber: '1,80g',
+    sodium: '112,00mg'
+  }
+}, {
+  id: 6,
+  name: 'Panettone Nestlé Alpino 400g',
+  price: '27,74',
+  brand: 'Nestle',
+  imgUrl: 'https://a-static.mlcdn.com.br/618x463/panettone-nestle-alpino-400g-nestle/lojasemix/1115p/077d017127dd09b081497c3f261e9d29.jpg',
+  nutriInfo: {
+    carb: '37,00g',
+    protein: '5,40g',
+    saturatedFat: '6,10g',
+    totalFat: '12,00g',
+    fiber: '1,90g',
+    sodium: '111,00mg'
+  }
+}, {
+  id: 7,
+  name: 'Panettone Frutas 400 gramas',
+  price: '9,90',
+  brand: 'Romanato',
+  imgUrl: 'https://a-static.mlcdn.com.br/618x463/panettone-frutas-400-gramas-romanato/nivalmix/10647/66c11c90c8e9d13f836c8001ad8c3169.jpg',
+  nutriInfo: {
+    carb: '39,00g',
+    protein: '5,00g',
+    saturatedFat: '5,10g',
+    totalFat: '10,00g',
+    fiber: '1,80g',
+    sodium: '112,00mg'
+  }
+}, {
+  id: 8,
+  name: 'Panettone Frutas Cristalizadas 400g',
+  price: '11,69',
+  brand: 'Tommy',
+  imgUrl:'https://a-static.mlcdn.com.br/618x463/panettone-frutas-cristalizadas-400g-tommy/docemalu/6070/daae99a78a4b1a57c8637e6cb4aff67e.jpg',
+  nutriInfo: {
+    carb: '37,00g',
+    protein: '4,00g',
+    saturatedFat: '4,50g',
+    totalFat: '8,00g',
+    fiber: '2,10g',
+    sodium: '114,00mg'
+  }
+}, {
+  id: 9,
+  name: 'Panettone Trufa 400g',
+  price: '19,39',
+  brand: 'Visconti',
+  imgUrl: 'https://a-static.mlcdn.com.br/618x463/panettone-trufa-400g-visconti/docemalu/16227/3bb8abf59ba8ba48ebc5bd535000892b.jpg',
+  nutriInfo: {
+    carb: '40,00g',
+    protein: '5,50g',
+    saturatedFat: '5,20g',
+    totalFat: '11,00g',
+    fiber: '1,90g',
+    sodium: '111,00mg'
+  }
+}, {
+  id: 10,
+  name: 'Chocotone Bauducco 908g',
+  price: '35,00',
+  brand: 'Bauducco',
+  imgUrl: 'https://a-static.mlcdn.com.br/618x463/chocotone-bauducco-908g/casasantaluzia2/6618/1d141a5c658fd32572fab7baa7ab0b9c.jpg',
+  nutriInfo: {
+    carb: '42,00g',
+    protein: '4,50g',
+    saturatedFat: '8,20g',
+    totalFat: '12,00g',
+    fiber: '1,90g',
+    sodium: '115,00mg'
+  }
+}, {
+  id: 11,
+  name: 'Panettone Chocolate e Baunilha 400g',
+  price: '19,39',
+  brand: 'Visconti',
+  imgUrl: 'https://a-static.mlcdn.com.br/618x463/panettone-chocolate-e-baunilha-400g-visconti/docemalu/16229/11856595292f05fdf0457bfd8d07d130.jpg',
+  nutriInfo: {
+    carb: '40,00g',
+    protein: '5,50g',
+    saturatedFat: '5,20g',
+    totalFat: '11,00g',
+    fiber: '1,90g',
+    sodium: '111,00mg'
+  }
 }];
 
 module.exports = panettones;
